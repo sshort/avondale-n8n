b48c1277324d72790c072c3ce55edc645407d8da
sshort <steve@shortcentral.com>
2026-03-16 15:56:52 +0000

    feat: added backups

---
+++ backups/20260315-124852/globals.sql
@@ -0,0 +1,11 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+\restrict uYPZXhkMMoqcKwNVbQyFsqLyoMaEwaEHolxiV98ZbQrJhskYlFuZWDwJIDKMfuh
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
